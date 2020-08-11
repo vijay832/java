9ef88276faa3c407825d0bed525fa5af7f9fa9bf
vijay832 <64659064+vijay832@users.noreply.github.com>
2020-08-11 14:41:19 -0500

    ArrayList follows insertion order

---
+++ Collection Framework/ArrayListDemo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class ArrayListDemo{
+public static void main(String []args){
+ArrayList<Integer> al=new ArrayList<Integer>();
+al.add(23);
+al.add(22);
+al.add(20);
+al.add(13);
+al.add(23);
+al.add(24);
+al.add(29);
+al.add(31);
+al.add(33);
+al.add(44);
+al.add(25);
+al.add(55);
+al.add(99);
+al.add(66);
+al.add(77);
+al.add(21);
+System.out.println(al);
+}
+}
