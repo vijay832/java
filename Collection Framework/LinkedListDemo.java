d86846ecad6995363f7c2a5b862e0966e9c89fcc
vijay832 <64659064+vijay832@users.noreply.github.com>
2020-08-11 14:44:21 -0500

    LinkedList follows insertion order
    Better for searching purpose

---
+++ Collection Framework/LinkedListDemo.java
@@ -0,0 +1,25 @@
+/*LinkedList follows insertion order and 
+   better for seaching purpose*/
+import java.util.*;
+class LinkedListDemo{
+public static void main(String []args){
+LinkedList<Integer> al=new LinkedList<Integer>();
+al.add(23);
+al.add(22);
+al.add(20);
+al.add(13);
+al.add(23);
+al.add(24);
+al.add(29);                  
+al.add(31);
+al.add(33);
+al.add(44);
+al.add(25);
+al.add(55);
+al.add(99);
+al.add(66);
+al.add(77);
+al.add(21);
+System.out.println(al);
+}
+}
