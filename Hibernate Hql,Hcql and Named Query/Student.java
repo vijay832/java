6ae7abeb9ddcc2a150ad1aa3943d37ddd26f5841
vijay832 <64659064+vijay832@users.noreply.github.com>
2020-09-26 16:24:05 -0500

    Hibernate

---
+++ Hibernate Hql,Hcql and Named Query/Student.java
@@ -0,0 +1,28 @@
+package com.school;
+
+public class Student {
+	
+	private int id;
+	private String sname;
+	private int marks;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public int getMarks() {
+		return marks;
+	}
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+}
