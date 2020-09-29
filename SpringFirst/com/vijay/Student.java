c98bfe178d98da648e27d20c269b16b4a6f3a4ca
vijay832 <64659064+vijay832@users.noreply.github.com>
2020-09-28 23:29:07 -0500

    Implemented with Map collection

--- SpringFirst/com/vijay/Student.java
+++ SpringFirst/com/vijay/Student.java
@@ -1,28 +1,64 @@
 package com.vijay;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class Student {
 
-private int id;
-private String name;
-private int marks;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
-public int getMarks() {
-	return marks;
-}
-public void setMarks(int marks) {
-	this.marks = marks;
-}
+	private int id;
+	private String name;
+	private int marks;
+	private List Course;
+	private Set StudentName;
+	private Map<String,String> CourseRegistration;
 
+	public Map<String, String> getCourseRegistration() {
+		return CourseRegistration;
+	}
+
+	public void setCourseRegistration(Map<String, String> courseRegistration) {
+		CourseRegistration = courseRegistration;
+	}
+
+	public Set getStudentName() {
+		return StudentName;
+	}
+
+	public void setStudentName(Set studentName) {
+		StudentName = studentName;
+	}
+
+	public List getCourse() {
+		return Course;
+	}
+
+	public void setCourse(List course) {
+		Course = course;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
 
 }
