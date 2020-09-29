5190aae37cdd29a287d604f50e07684fdfdeba4b
vijay832 <64659064+vijay832@users.noreply.github.com>
2020-09-28 23:27:58 -0500

    implemented Map

--- SpringFirst/com/vijay/MainLogic.java
+++ SpringFirst/com/vijay/MainLogic.java
@@ -1,5 +1,11 @@
 package com.vijay;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
@@ -12,6 +18,25 @@ public class MainLogic {
 		Object o =f.getBean("rahul");
 		Student s=(Student) o;
 		System.out.println(s.getId()+"  "+s.getName()+"  "+s.getMarks());
+		List x=s.getCourse();
+		Iterator i=x.iterator();
+		
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+		
+		Set y=s.getStudentName();
+		Iterator j=y.iterator();
+		
+		while(j.hasNext()) {
+			System.out.println(j.next());
+		}
+		Set<Entry<String, String>> set=s.getCourseRegistration().entrySet();  
+	    Iterator<Entry<String, String>> itr=set.iterator();  
+	    while(itr.hasNext()){  
+	        Entry<String,String> entry=itr.next();  
+	        System.out.println("StudentName: "+entry.getKey()+" Course registered:"+entry.getValue());  
+	}
 	}
 
 }
